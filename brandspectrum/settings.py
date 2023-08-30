cfbf89e9959bd7acccd4e23d4a6a16913a61e4a2
Mustafa <mustafazaidi840@gmail.com>
2023-08-30 16:17:22 +0500

    feat: Connect Django project to PostgreSQL database

--- brandspectrum/settings.py
+++ brandspectrum/settings.py
@@ -77,12 +77,25 @@ WSGI_APPLICATION = 'brandspectrum.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mydb',
+        'USER': 'mustafa',
+        'PASSWORD': '1214',
+        'HOST': 'localhost',  
+        'PORT': '5432',
     }
 }
+
+
 
 
 # Password validation
